626ee8b65e27bcc6c9b446c59bc6f682e3c5568c
Hafizjavaid <hafizjaidi786@gmail.com>
2020-10-04 23:14:16 +0500

    Fist

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,75 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-app-bar flat app color="white">
+      <div class="d-flex align-center">
+        <v-card color="grey lighten-4" outlined width="300px" class="pa-2 ml-4">
+          <v-layout>
+            <v-flex>
+              <v-icon color="grey">mdi-magnify</v-icon>
+              <span class="ml-2 grey--text">Search</span>
+            </v-flex>
+            <v-flex class="text-right">
+              <v-icon color="grey">mdi-microphone-outline</v-icon>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn dark depressed color="#E7596F" class="text-none mr-5">
+        <span>
+          <v-icon class="mr-2">mdi-plus</v-icon>Create New Post
+        </span>
+      </v-btn>
+      <v-btn icon class="mr-5">
+        <v-icon class="mr-2">mdi-email-outline</v-icon>
+      </v-btn>
+      <v-btn icon class="mr-5">
+        <v-icon class="mr-2">mdi-heart-outline</v-icon>
+      </v-btn>
+      <v-avatar size="30">
+        <img
+          lazy-src="https://images.unsplash.com/photo-1549068106-b024baf5062d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+          src="https://images.unsplash.com/photo-1549068106-b024baf5062d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80"
+          alt="John"
+        />
+      </v-avatar>
+      <v-icon>mdi-menu-down</v-icon>
+    </v-app-bar>
+    <Leftnav />
+    <Rightnav />
+
+    <v-main>
+      <Page />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Leftnav from "./components/Leftnav.vue";
+import Rightnav from "./components/Rightnav.vue";
+import Page from "./components/Mainpage";
 
-#nav {
-  padding: 30px;
-}
+export default {
+  name: "App",
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  components: {
+    Leftnav,
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+    Rightnav,
+    Page,
+  },
+
+  data: () => ({
+    drawer: true,
+    group: null,
+    //
+  }),
+  watch: {
+    group() {
+      this.drawer = false;
+    },
+  },
+};
+</script>
---
+++ src/components/Mainpage.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-container class="pa-10">
+    <stories-section />
+    <feed-section />
+  </v-container>
+</template>
+
+<script>
+import StoriesSection from "./Stories";
+import FeedSection from "./Feeds";
+export default {
+  components: {
+    StoriesSection,
+    FeedSection,
+  },
+};
+</script>
+
+<style>
+</style>
